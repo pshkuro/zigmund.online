266d8cc1fc72a221fe534e7c3b4c4d57ec8275c9
Полина <polina110297@yandex.ru>
2020-08-09 21:39:52 +0300

    feat(search-form): connect to redux & prettify

--- src/components/search-form/search-form.tsx
+++ src/components/search-form/search-form.tsx
@@ -1,34 +1,35 @@
-import React, { useState } from 'react';
-import { Theme, makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import InputBase from '@material-ui/core/InputBase';
-import IconButton from '@material-ui/core/IconButton';
-import SearchIcon from '@material-ui/icons/Search';
+import React, { useState, Dispatch } from "react";
+import { connect } from "react-redux";
+import InputBase from "@material-ui/core/InputBase";
+import IconButton from "@material-ui/core/IconButton";
+import SearchIcon from "@material-ui/icons/Search";
+import Paper from "@material-ui/core/Paper";
+import { Theme, makeStyles } from "@material-ui/core/styles";
+import { ActionCreator } from "../../redux/reducer";
 
 interface Props {
-  onSubmit: (value: string) => void,
+  onSubmit: (value: string) => void;
 }
 
 const VALIDATORS = {
   minLength: 3,
   maxLength: 30,
-}
+};
 
 const useStyles = makeStyles((theme: Theme) => ({
   searchForm: {
     padding: theme.spacing(2, 4),
     display: `flex`,
     alignItems: `center`,
-    marginBottom: theme.spacing(5)
-  }, 
+    marginBottom: theme.spacing(5),
+  },
   searchInput: {
     ...theme.typography.h5,
     flex: 1,
   },
 }));
 
-
-export default function SearchForm({onSubmit}: Props): JSX.Element {
+function SearchForm({ onSubmit }: Props): JSX.Element {
   const classes = useStyles();
   const [value, setValue] = useState(``);
 
@@ -36,10 +37,15 @@ export default function SearchForm({onSubmit}: Props): JSX.Element {
     evt.preventDefault();
     onSubmit(value);
   };
-  const handleInputChange = (evt: React.ChangeEvent<HTMLInputElement>) => setValue(evt.target.value);
+  const handleInputChange = (evt: React.ChangeEvent<HTMLInputElement>) =>
+    setValue(evt.target.value);
 
   return (
-    <Paper component="form" className={classes.searchForm} onSubmit={handleSubmit}>
+    <Paper
+      component="form"
+      className={classes.searchForm}
+      onSubmit={handleSubmit}
+    >
       <InputBase
         className={classes.searchInput}
         placeholder="Search Repository"
@@ -56,3 +62,11 @@ export default function SearchForm({onSubmit}: Props): JSX.Element {
     </Paper>
   );
 }
+
+export const mapDispatchToProps = (dispatch: Dispatch<any>) => ({
+  onSubmit(org: string) {
+    dispatch(ActionCreator.getRepositories(org));
+  },
+});
+
+export default connect(null, mapDispatchToProps)(SearchForm);
