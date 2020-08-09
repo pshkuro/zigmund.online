85d401a0c8ffa04dcca04c14c1990a7b9a5cf6c3
Полина <polina110297@yandex.ru>
2020-08-09 21:40:12 +0300

    feat(repo-card): connect to redux & prettify

--- src/components/repo-list/repo-card.tsx
+++ src/components/repo-list/repo-card.tsx
@@ -1,55 +1,79 @@
-import React from 'react';
-import { Theme, makeStyles } from '@material-ui/core/styles';
-import StarIcon from '@material-ui/icons/Star';
-import VisibilityIcon from '@material-ui/icons/Visibility';
-import SwapCallsIcon from '@material-ui/icons/SwapCalls';
-import Typography from '@material-ui/core/Typography';
-import Link from '@material-ui/core/Link';
-import Chip from '@material-ui/core/Chip';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import {Repository as Props} from "../../models/repository";
+import React from "react";
+import Chip from "@material-ui/core/Chip";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import Link from "@material-ui/core/Link";
+import StarIcon from "@material-ui/icons/Star";
+import SwapCallsIcon from "@material-ui/icons/SwapCalls";
+import { Theme, makeStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import VisibilityIcon from "@material-ui/icons/Visibility";
+import { Repository as Props } from "../../core/models/repository";
+import Box from "@material-ui/core/Box";
 
 const STATISTIC_ICON_MAPPING = new Map([
-  ['forks', SwapCallsIcon],
-  ['watchers', VisibilityIcon],
-  ['stargazers', StarIcon],
+  ["forks", SwapCallsIcon],
+  ["watchers", VisibilityIcon],
+  ["stargazers", StarIcon],
 ]);
 
 const useStyles = makeStyles((theme: Theme) => ({
+  card: {
+    height: "100%",
+  },
+  cardContent: {
+    display: "flex",
+    flexDirection: "column",
+    height: "100%",
+  },
+  chipList: {
+    marginTop: "auto",
+  },
   chip: {
     marginRight: theme.spacing(1),
-  } 
+  },
 }));
 
 export default function RepoCard({
   name,
   description,
   url,
-  statistics
+  statistics,
 }: Props): JSX.Element {
   const classes = useStyles();
   const statisticInfo = Object.entries(statistics).map(([name, count]) => {
     const Icon = STATISTIC_ICON_MAPPING.get(name);
     return (
-      <Chip 
-        className={classes.chip} 
+      <Chip
+        className={classes.chip}
         variant="outlined"
         icon={Icon && <Icon />}
         label={count}
-        key={name} />
+        key={name}
+      />
     );
   });
 
   return (
-    <Card>
-      <CardContent>
+    <Card className={classes.card}>
+      <CardContent className={classes.cardContent}>
         <Typography gutterBottom variant="h5" component="h2">
-          <Link href={url} target="_blank">{name}</Link>
+          <Link href={url} target="_blank">
+            {name}
+          </Link>
         </Typography>
-        <Typography variant="body2" color="textSecondary" component="p" paragraph>{description}</Typography>
-        {statisticInfo}
-      </CardContent>    
-  </Card>
+        {description && (
+          <Typography
+            variant="body2"
+            color="textSecondary"
+            component="p"
+            paragraph
+          >
+            {description}
+          </Typography>
+        )}
+        <div className={classes.chipList}>{statisticInfo}</div>
+      </CardContent>
+    </Card>
   );
 }
