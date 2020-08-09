9a443ad91c568178a3f3fb294d43d6d0043a7ede
Полина <polina110297@yandex.ru>
2020-08-09 21:43:55 +0300

    feat(app): build layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,29 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Box from "@material-ui/core/Box";
+import Container from "@material-ui/core/Container";
+import SearchForm from "./components/search-form/search-form";
+import RepoList from "./components/repo-list/repo-list";
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <React.Fragment>
+      <CssBaseline />
+      <div className="App">
+        <Container>
+          <Box
+            paddingY={3}
+            display="flex"
+            flexDirection="column"
+            minHeight="100vh"
+          >
+            <SearchForm />
+            <RepoList />
+          </Box>
+        </Container>
+      </div>
+    </React.Fragment>
   );
 }
 
