77d74aac5bc44b4ff0187b8f5d0dadaefe88ce2f
Полина <polina110297@yandex.ru>
2020-08-09 21:41:50 +0300

    feat: create store & saga middleware & connect store to project

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import { Provider } from "react-redux";
+import createSagaMiddleware from "redux-saga";
+import * as serviceWorker from "./serviceWorker";
+import "./index.css";
+import App from "./App";
+import reducer from "./redux/reducer";
+import rootSaga from "./redux/sagas/root-saga";
+
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(
+  reducer,
+  composeWithDevTools(applyMiddleware(sagaMiddleware))
+);
+
+sagaMiddleware.run(rootSaga);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
